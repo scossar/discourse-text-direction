5ce5e60adfbe0737316b37ca16dff58be7fb5b82
scossar <scossar3@gmail.com>
2016-08-13 17:08:40 -0700

    Make it work

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: discourse-text-direction
+# about: A Discourse plugin to set the text-direction with BBCode tags.
+# version: 0.1
+# authors: scossar
+# url: https://github.com/scossar/discourse-text-direction
+
+register_asset 'stylesheets/text-direction.scss'
